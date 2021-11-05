83cdd4358a109d8f473e47d7ecd0053622431eb8
Eugene Edwardsson <eugeneberezkin87@gmail.com>
2021-11-05 20:11:27 +0300

    Changes HW

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+
+
+
+class Student {
+    var name : String
+    var surname : String
+    var yearOfBorn : Int
+    var mark : Double
+    
+    init(name: String, surname : String, yearOfBorn : Int, mark : Double) {
+        self.name = name
+        self.surname = surname
+        self.yearOfBorn = yearOfBorn
+        self.mark = mark
+    }
+    func objectStudent(){
+        let object = Student(name: "Mike", surname: "Lorne", yearOfBorn: 30, mark: 2.0)
+    
+        print(object)
+    }
+    
+}
+
